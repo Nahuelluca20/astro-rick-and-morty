3487925727328f4ef3df92a164feeab9463dda16
Nahuel Luca <nahuelluca20@gmail.com>
2023-09-04 20:22:44 -0300

    add: pagination

--- src/store/paginationStore.ts
+++ src/store/paginationStore.ts
@@ -1,7 +1,10 @@
 import { atom } from 'nanostores';
 
 export const paginationUrl = atom("https://rickandmortyapi.com/api/character");
-export const paginationNumber = atom(0);
+export const paginationNumber = atom(1);
 export function changePaginationUrl(url: string) {
   paginationUrl.set(url);
+}
+export function changePaginatonNumber(number: number) {
+  paginationNumber.set(number);
 }
