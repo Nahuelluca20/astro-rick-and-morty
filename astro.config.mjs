4342952d599ab32f5e41ca6b7466bae82c621b3f
Nahuel Luca <nahuelluca20@gmail.com>
2023-09-04 01:29:39 -0300

    add: adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import nodejs from '@astrojs/node';
 import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  adapter: nodejs(),
+  output: 'hybrid',
   integrations: [tailwind(), react()],
 });
